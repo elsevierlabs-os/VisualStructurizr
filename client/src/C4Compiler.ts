d439db3c68805271b9674cf55c516d3cce78299d
Gerry Rohling <g.rohling@elsevier.com>
2024-04-21 11:35:09 +0100

    Trying to create an MX file but issues with elkjs worker

--- client/src/C4Compiler.ts
+++ client/src/C4Compiler.ts
@@ -1,17 +1,23 @@
 import * as vscode from 'vscode';
-import { C4Interpreter, C4View, C4Workspace, StructurizrLexer, StructurizrParser } from "structurizr-parser";
+import { C4ComponentView, C4ContainerView, C4Interpreter, C4SystemContextView, C4SystemLandscapeView, C4View, C4Workspace, StructurizrLexer, StructurizrParser } from "structurizr-parser";
 import { MxBuilder } from 'mxbuilder';
+import { posix } from 'path';
 
 export class C4Compiler {
 
-    private uri: vscode.Uri;
+    private dslUri: vscode.Uri;
+    private dslFilename: string;
+    private dslDirname: string;
+
     constructor() {
     }
 
-    async process(uri: vscode.Uri) : Promise<boolean> {
-        console.log(`C4Compiler: I am here to compile the source at ${uri}`);
-        this.uri = uri;
-        const readData = await vscode.workspace.fs.readFile(uri);
+    async process(dslUri: vscode.Uri) : Promise<boolean> {
+        console.log(`C4Compiler: I am here to compile the source at ${dslUri}`);
+        this.dslUri = dslUri;
+        this.dslFilename = posix.basename(dslUri.path,'.dsl');
+        this.dslDirname = posix.dirname(dslUri.path);
+        const readData = await vscode.workspace.fs.readFile(dslUri);
         const readStr = Buffer.from(readData).toString('utf8');
         console.log(readStr);
         let lexingResult = StructurizrLexer.tokenize(readStr);
@@ -30,16 +36,16 @@ export class C4Compiler {
                 console.log(`We have a view of type: ${view.Type}`);
                 switch (view.Type) {
                     case "SystemLandscape":
-                        this.createSystemLandscapeView(view, c4workspace);
+                        this.createSystemLandscapeView(view as C4SystemLandscapeView, c4workspace);
                         break;
                     case "SystemContext":
-                        this.createSystemContextView(view, c4workspace);
+                        this.createSystemContextView(view as C4SystemContextView, c4workspace);
                         break;
                     case "Container":
-                        this.createContainerView(view, c4workspace);
+                        this.createContainerView(view as C4ContainerView, c4workspace);
                         break;
                     case "Component":
-                        this.createComponentView(view, c4workspace);
+                        this.createComponentView(view as C4ComponentView, c4workspace);
                         break;
                     default:
                         break;
@@ -59,25 +65,30 @@ export class C4Compiler {
         return true;
 	}
 
-    createSystemLandscapeView(view: C4View, c4workspace: C4Workspace) {
+    async createSystemLandscapeView(view: C4SystemLandscapeView, c4workspace: C4Workspace) {
         var mx = new MxBuilder();
         const dwg = mx.toDiagram();
         throw new Error('Method not implemented.');
     }
 
-    createSystemContextView(view: C4View, c4workspace: C4Workspace) {
+    async createSystemContextView(view: C4SystemContextView, c4workspace: C4Workspace) {
+        var mx = new MxBuilder();
+        const dwg = await mx.toDiagram();
+        const writeData = Buffer.from(dwg, 'utf8');
+        const rootSystem = view.SystemId;
+        const targetFile = posix.join(this.dslDirname, this.dslFilename, "-", rootSystem, "-Context.drawio");
+        const targetUri = vscode.Uri.file(targetFile);
+        await vscode.workspace.fs.writeFile(targetUri, writeData);
+        await vscode.commands.executeCommand("vscode.open", targetUri);
+    }
+
+    async createContainerView(view: C4ContainerView, c4workspace: C4Workspace) {
         var mx = new MxBuilder();
         const dwg = mx.toDiagram();
         throw new Error('Method not implemented.');
     }
 
-    createContainerView(view: C4View, c4workspace: C4Workspace) {
-        var mx = new MxBuilder();
-        const dwg = mx.toDiagram();
-        throw new Error('Method not implemented.');
-    }
-
-    createComponentView(view: C4View, c4workspace: C4Workspace) {
+    async createComponentView(view: C4ComponentView, c4workspace: C4Workspace) {
         var mx = new MxBuilder();
         const dwg = mx.toDiagram();
         throw new Error('Method not implemented.');
