8e1a8b6fb748000c2fb380c2055ffb1573ed3384
Gerry Rohling <g.rohling@elsevier.com>
2024-04-28 16:05:05 +0100

    Seems to be able to draw context diagrams now

--- client/src/C4Compiler.ts
+++ client/src/C4Compiler.ts
@@ -1,7 +1,8 @@
 import * as vscode from 'vscode';
-import { C4ComponentView, C4ContainerView, C4Interpreter, C4SystemContextView, C4SystemLandscapeView, C4View, C4Workspace, StructurizrLexer, StructurizrParser } from "structurizr-parser";
+import { C4ComponentView, C4ContainerView, C4Element, C4Interpreter, C4Person, C4Relationship, C4SoftwareSystem, C4SystemContextView, C4SystemLandscapeView, C4View, C4Workspace, StructurizrLexer, StructurizrParser } from "structurizr-parser";
 import { MxBuilder } from 'mxbuilder';
 import { posix } from 'path';
+import { mkdirSync } from 'fs';
 
 export class C4Compiler {
 
@@ -12,10 +13,10 @@ export class C4Compiler {
     constructor() {
     }
 
-    async process(dslUri: vscode.Uri) : Promise<boolean> {
+    async process(dslUri: vscode.Uri): Promise<boolean> {
         console.log(`C4Compiler: I am here to compile the source at ${dslUri}`);
         this.dslUri = dslUri;
-        this.dslFilename = posix.basename(dslUri.path,'.dsl');
+        this.dslFilename = posix.basename(dslUri.path, '.dsl');
         this.dslDirname = posix.dirname(dslUri.path);
         const readData = await vscode.workspace.fs.readFile(dslUri);
         const readStr = Buffer.from(readData).toString('utf8');
@@ -23,7 +24,7 @@ export class C4Compiler {
         let lexingResult = StructurizrLexer.tokenize(readStr);
         StructurizrParser.input = lexingResult.tokens;
         const cst = StructurizrParser.workspaceWrapper();
-        if (StructurizrParser.errors.length > 0) { 
+        if (StructurizrParser.errors.length > 0) {
             return false;
         }
         try {
@@ -52,9 +53,9 @@ export class C4Compiler {
                 }
             }
 
-        } catch (e:any) {
-            if (e.message){
-                vscode.window.showErrorMessage('Error converting to C4', {detail: e.message, modal: true});
+        } catch (e: any) {
+            if (e.message) {
+                vscode.window.showErrorMessage('Error converting to C4', { detail: e.message, modal: true });
             } else {
                 vscode.window.showErrorMessage('Unknown error converting to C4');
             }
@@ -63,7 +64,7 @@ export class C4Compiler {
 
 
         return true;
-	}
+    }
 
     async createSystemLandscapeView(view: C4SystemLandscapeView, c4workspace: C4Workspace) {
         var mx = new MxBuilder();
@@ -71,12 +72,100 @@ export class C4Compiler {
         throw new Error('Method not implemented.');
     }
 
+    // SYSTEM CONTEXT IS All PEOPLE, SOFTWARE SYSTEMS AND RELATIONSHIPS DIRECTLY LINKED TO PROVIDED ROOT SYSTEM
     async createSystemContextView(view: C4SystemContextView, c4workspace: C4Workspace) {
+        // We need to build a list of linked entities
+        const entityList: C4Element[] = [];
+        const relationshipList: ViewRelationship[] = [];
+        // We can add the root software system right away - this will always then be the [0] entry in the entity list
+        const rootSystemTree = c4workspace.Model.findElement(view.SystemId);
+        if (rootSystemTree.length > 0) {
+            entityList.push(rootSystemTree[0]);
+            // Now we need to look at all relationships that start or end with the root system
+            // First those that start with the root system
+            entityList[0].Relationships.forEach(r => {
+                let targetTree = c4workspace.Model.findElement(r.TargetId);
+                if (targetTree.length > 0) {
+                    if (targetTree[0] instanceof C4SoftwareSystem || targetTree[0] instanceof C4Person) {
+                        relationshipList.push(new ViewRelationship(entityList[0].Id, targetTree[0].Id, r.Description, r.Technology));
+                        if (!entityList.includes(targetTree[0])) { entityList.push(targetTree[0]); };
+                    }
+                }
+            });
+            // Then we need to find those that end with the root system - could we combine this with the earlier?
+            c4workspace.Model.People.forEach(p => p.Relationships.forEach(r => {
+                let targetTree = c4workspace.Model.findElement(r.TargetId);
+                if (targetTree.length > 0 && targetTree[0] instanceof C4SoftwareSystem && targetTree[0].Id === entityList[0].Id) {
+                    relationshipList.push(new ViewRelationship(p.Id, targetTree[0].Id, r.Description, r.Technology));
+                    if (!entityList.includes(p)) { entityList.push(p); };
+                };
+            }));
+            c4workspace.Model.SoftwareSystems.forEach(ss => ss.Relationships.forEach(r => {
+                let targetTree = c4workspace.Model.findElement(r.TargetId);
+                if (targetTree.length > 0 && targetTree[0] instanceof C4SoftwareSystem && targetTree[0].Id === entityList[0].Id) {
+                    relationshipList.push(new ViewRelationship(ss.Id, targetTree[0].Id, r.Description, r.Technology));
+                    if (!entityList.includes(ss)) { entityList.push(ss); };
+                };
+            }));
+            c4workspace.Model.Groups.forEach(g => {
+                g.People.forEach(p => {
+                    p.Relationships.forEach(r => {
+                        let targetTree = c4workspace.Model.findElement(r.TargetId);
+                        if (targetTree.length > 0 && targetTree[0] instanceof C4SoftwareSystem && targetTree[0].Id === entityList[0].Id) {
+                            relationshipList.push(new ViewRelationship(p.Id, targetTree[0].Id, r.Description, r.Technology));
+                            if (!entityList.includes(p)) { entityList.push(p); };
+                        };
+                    });
+                });
+                g.SoftwareSystems.forEach(ss => {
+                    ss.Relationships.forEach(r => {
+                        let targetTree = c4workspace.Model.findElement(r.TargetId);
+                        if (targetTree.length > 0 && targetTree[0] instanceof C4SoftwareSystem && targetTree[0].Id === entityList[0].Id) {
+                            relationshipList.push(new ViewRelationship(ss.Id, targetTree[0].Id, r.Description, r.Technology));
+                            if (!entityList.includes(ss)) { entityList.push(ss); };
+                        };
+                    });
+                });
+            });
+        }
+
         var mx = new MxBuilder();
+
+        // Place Entities 
+        c4workspace.Model.SoftwareSystems.forEach(ss => {
+            if (entityList.includes(ss)) {
+                mx.placeSoftwareSystem(ss.Name, ss.Description, ss.Id);
+            }
+        });
+        c4workspace.Model.People.forEach(p =>{
+            if (entityList.includes(p)) {
+                mx.placePerson(p.Name, p.Description, p.Id);
+            }
+        });
+        c4workspace.Model.Groups.forEach(g => {
+            mx.placeGroupBoundary(g.Name, "", g.Id);
+            g.People.forEach(p => {
+                if (entityList.includes(p)) {
+                    mx.placePerson(p.Name, p.Description, p.Id, g.Id);
+                }
+            });
+            g.SoftwareSystems.forEach(ss => {
+                if (entityList.includes(ss)) {
+                    mx.placeSoftwareSystem(ss.Name, ss.Description, ss.Id, g.Id);
+                }
+            });
+        });
+
+        // Place Relationships
+        relationshipList.forEach(r => {
+            mx.placeRelationship(r.description, r.technology, r.sourceId, r.targetId);
+        });
+
+        // Add legend?? Should be default of mx maybe?
+        // Create Drawing
         const dwg = await mx.toDiagram();
         const writeData = Buffer.from(dwg, 'utf8');
-        const rootSystem = view.SystemId;
-        const targetFile = posix.join(this.dslDirname, this.dslFilename + "-" + rootSystem + "-Context.drawio");
+        const targetFile = posix.join(this.dslDirname, this.dslFilename + "-" + entityList[0].Name + "-Context.drawio");
         const targetUri = vscode.Uri.file(targetFile);
         await vscode.workspace.fs.writeFile(targetUri, writeData);
         await vscode.commands.executeCommand("vscode.open", targetUri);
@@ -93,4 +182,20 @@ export class C4Compiler {
         const dwg = mx.toDiagram();
         throw new Error('Method not implemented.');
     }
+}
+
+class ViewRelationship {
+
+    sourceId: string;
+    targetId: string;
+    description: string;
+    technology: string;
+
+    constructor(src: string, tgt: string, desc?: string, tech?: string) {
+        this.sourceId = src;
+        this.targetId = tgt;
+        this.description = desc;
+        this.technology = tech;
+    }
+
 }
